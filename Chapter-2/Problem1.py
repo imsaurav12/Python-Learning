9eb86bd96d618b35d4dd0c6b014b241d3d016f10
Saurav Raj <er.sauravraj12@gmail.com>
2026-06-20 21:32:52 +0530

    finished chapter 2

---
+++ Chapter-2/Problem1.py
@@ -0,0 +1,14 @@
+# # to add two numbers
+
+# a = int(input("Enter number 1: "))
+# b = int(input("Enter number 2: "))
+
+# print("Sum is: " ,a+b)
+
+# finding remainder
+
+a = int(input("Enter a number : "))
+z = int(input("Enter z number : "))
+
+ans = a % z ; 
+print("When",a, "is divided by ",z,"we get" ,ans, "as answer")
